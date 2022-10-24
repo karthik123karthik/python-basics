8a9bdc187862c3b0c46beb8cb47d2af243f4d87d
karthik g.k <kar221694@gmail.com>
2022-10-24 10:34:30 -0400

    oops modified

--- oops.py
+++ oops.py
@@ -1,19 +1,50 @@
-# creating a class
-class Customer:
-    def __init__(self, name, membership):
-        self.name = name
-        self.membership = membership
+#create a class
+class Computer:
+    #constructor
+    electronic_device = True # static variables
+    def __init__(self,cpu,ram):
+        self.cpu = cpu
+        self.ram = ram
 
-    def take_membership(self):
-        self.membership = True
+    def config(self):
+        print(self.cpu, self.ram)
 
-# creating an object
+    @classmethod
+    def get_info(cls):
+        return cls.electronic_device
+    
+    @staticmethod
+    def say_hi():
+        print("hello world")
+    
+    # instant variable and static variable
+    # static variable are common for all the objects
 
+ 
+    def compare(self, other):
+        if self.cpu == other.cpu:
+            return True
+        else:
+            return False            
 
-c1 = Customer("karthik", False)
-print(c1.membership)
-# take membership
-c1.take_membership()
-print(c1.membership)
+comp1 = Computer("i5",16)
+# print(type(comp1))
+comp1.config()
+print(id(comp1))  #address of comp1 object
+
+# comparing two objects
+comp2 = Computer("i3",4)
+print(Computer.electronic_device)
+comp2.electronic_device = False  # donot change static value 
+print(comp2.electronic_device)
+if comp2.compare(comp1):
+    print("they are same")
+else:
+    print("they are different")
+
+# different types of methods static methods, instance methods, class method 
+print(comp1.get_info())
+comp2.say_hi()
+
 
 
