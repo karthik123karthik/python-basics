398464ca44d50f5f8550562b4cfb525fe4e83d51
Karthik G K <84716922+karthik123karthik@users.noreply.github.com>
2022-12-11 10:53:00 -0500

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+# if else statement is used in decision making
+# first if will run then elif the else
+# eg:
+
+age = 20
+if age>20:
+  print("you can marry")
+elif age>=18 and age<=20:
+  print("please wait for some time")
+else:
+  print("you are not allowed to marry")
