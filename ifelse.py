90858c2e774b7ffda6e395fa9306ae9fcd032471
Karthik G K <84716922+karthik123karthik@users.noreply.github.com>
2022-12-13 21:09:01 +0530

    Update ifelse.py

--- ifelse.py
+++ ifelse.py
@@ -9,3 +9,14 @@ elif age>=18 and age<=20:
   print("please wait for some time")
 else:
   print("you are not allowed to marry")
+
+match age:
+     Case 4:
+            print("hello baby boy")
+     Case 10:
+            Print("hello child")
+     Case 15:
+            Print("hello teenager")
+     Case _:
+           print(" hello adult")
+   
