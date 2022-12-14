915132e7191f9cf85bb0aa17ee311fcc48327c26
Karthik G K <84716922+karthik123karthik@users.noreply.github.com>
2022-12-14 11:35:23 -0500

    Create rangefunc.py

---
+++ rangefunc.py
@@ -0,0 +1,10 @@
+# range function in python
+
+for i in range(10):
+    print(i,end=" ")  # prints from 0 to 9
+
+# range(start, end but not included, steps how many hops)
+
+for i in range(0, 10, 2):
+    print(i,end=" ") # prints 0 2 4 6 8 
+
