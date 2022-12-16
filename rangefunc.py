1ab1d3588f43654ee9491b68810e630714267184
Karthik G K <84716922+karthik123karthik@users.noreply.github.com>
2022-12-16 20:36:10 +0530

    Update rangefunc.py

--- rangefunc.py
+++ rangefunc.py
@@ -7,4 +7,11 @@ for i in range(10):
 
 for i in range(0, 10, 2):
     print(i,end=" ") # prints 0 2 4 6 8 
+for i in range(20):
+    if i==2: 
+       Continue
+    if i%2== 0: 
+       break
+    print(i)
+       
 
