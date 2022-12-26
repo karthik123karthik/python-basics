6f06566269a19c6553cd413c902c573ac79f08c9
Karthik G K <84716922+karthik123karthik@users.noreply.github.com>
2022-12-26 17:33:44 +0530

    Create docsandpep8.py

---
+++ docsandpep8.py
@@ -0,0 +1,9 @@
+import this # this will print the zen of python which is like poem talking about best practices in python
+
+def square(n):
+    '''this will take an integer as an input and return it's square as an output'''
+    return n*n
+
+#doc strings are not ignored by compiler they are written next to the function name
+print(square.__doc__)
+#pep8 are the guidlines to enhance python programs
