26ae17ec5ed55ff522cfc62d4de48483ffb80b1b
Karthik G K <84716922+karthik123karthik@users.noreply.github.com>
2023-01-09 22:20:44 +0530

    Create virtual.py

---
+++ virtual.py
@@ -0,0 +1,9 @@
+virtual environment.
+it is a tool used by programmers to create different python environments inside a single computer.
+where you can have packages of different versions and dependencies.
+
+command to create - 
+python -m venv myenv # this command will create a python environment with python installed and name myenv
+
+command to activate
+myenv\Scripts\activate.bat 
