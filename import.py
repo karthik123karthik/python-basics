2d2220b5f741e49f37d11985645f813cf4ab665c
Karthik G K <84716922+karthik123karthik@users.noreply.github.com>
2023-01-10 20:58:23 +0530

    Create import.py

---
+++ import.py
@@ -0,0 +1,6 @@
+#import math this is to import math module 
+#import math * this will import everything inside current folder
+#from math import sqrt,pi this will import only specific values
+import math as m
+
+print(dir(m)) #gives the list of everything inside this module
