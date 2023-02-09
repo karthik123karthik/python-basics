375a3674793e69dcc6d9f5ba92c06af7626eb90d
Karthik G K <84716922+karthik123karthik@users.noreply.github.com>
2023-02-09 22:29:05 +0530

    Create Dunder.py

---
+++ Dunder.py
@@ -0,0 +1,8 @@
+class Employee:
+     #this is called magic/dunder method and is used to do some crazy things
+      def __str__(self):
+          return "this is employee class"
+
+
+e1= Employee()
+print(e1)
