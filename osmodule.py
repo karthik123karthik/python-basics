3cafa9737dead03cc2075b301f6374ebee964eff
Karthik G K <84716922+karthik123karthik@users.noreply.github.com>
2023-02-10 22:27:40 +0530

    Update osmodule.py

--- osmodule.py
+++ osmodule.py
@@ -3,6 +3,7 @@ import os
 path = 'C:/Users/user/Desktop/os module/directory/'
 files = os.listdir(path)
 for index, file in enumerate(files):
+  If file.endsWith(".py"):
     src = path+file
     dest = path+f"{index}.py"
     os.rename(src, dest)
