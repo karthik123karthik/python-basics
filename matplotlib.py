b8265ca686d6bfe02167c4951b05f295dd5bea53
Karthik G K <84716922+karthik123karthik@users.noreply.github.com>
2023-03-23 13:18:45 +0530

    Update matplotlib.py

--- matplotlib.py
+++ matplotlib.py
@@ -2,6 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('./deeplearning.mplstyle')
 
+import numpy as np
+%matplotlib widget
+import matplotlib.pyplot as plt
+from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+plt.style.use('./deeplearning.mplstyle')
+
 # using numpy to create dataset
 x_train = np.array([1.0, 2.0])
 y_train = np.array([300.0, 500.0])
