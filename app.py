7564d3f1ce20bbc7d82ce8685211fb03994cded7
Karthik G K <kar221694@gmail.com>
2023-06-18 19:33:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,38 +1,20 @@
-import random
-import string
-
-message = input("Enter the message you want to send: ")
-characters = list(string.printable)
-
-def encrypt(msg):
-    n = len(msg)
-    if n<3:
-         return msg[::-1]
-    else:
-        front = ""
-        back = ""
-        for _ in range(3):
-            front+=random.choice(characters)
-            back+=random.choice(characters)
-        newmsg = msg[1:]+msg[0]
-        #print(newmsg)
-        newmsg = front + newmsg + back
-        return newmsg
+from flask import Flask;
 
 
-def decrypt(msg):
-    n = len(msg)
-    if n<3:
-        return msg[::-1]
-    else:
-        actualmsg = msg[3:n-4]
-        #print(actualmsg)
-        actualmsg = msg[n-4]+ actualmsg
-        return actualmsg
+app = Flask(__name__)
 
+
+@app.route("/hello")
+def  hello():
+    with open("hello.txt", 'w') as hello:
+        hello.write("hello world")      
+    return "updated the file"
+
+@app.route('/')
+def home():
+    with open("hello.txt", 'r') as hello:
+        return hello.read()
 
 
 if __name__ == '__main__':
-    encryptedmsg = encrypt(message)
-    #print(encryptedmsg)
-    print("THE DECRYPTED MESSAGE IS :",decrypt(encryptedmsg))
+    app.run(host='127.0.0.1', port=3000, debug=True)
