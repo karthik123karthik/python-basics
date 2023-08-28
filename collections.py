722cbfd1970e1d4fec9d27e8a94eec7eef0028be
Karthik G K <kar221694@gmail.com>
2023-08-28 19:15:50 +0530

    Update collections.py

--- collections.py
+++ collections.py
@@ -1,5 +1,13 @@
-# collection : Counter, namedtuple, OrderedDict, defaultdict, deque
-from collections import Counter
-a = "aaaaaabbbbbccccc"
-my_counter = Counter(a)
-print(my_counter)
+from collections import Counter, deque, namedtuple;
+
+# create a counter object
+ans = '123456665233445234623466464'
+counterobj = Counter(ans)
+print(counterobj)
+
+# creating a stack using deque
+stack = deque([])
+stack.append(1) # append to the left
+print(stack.pop()) #delete to the left
+stack.appendleft(1)
+print(stack.popleft())
